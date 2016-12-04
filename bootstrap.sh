9a3ddbec0b51f02bb08c0d7314670f2d61e7ad82
Sean Esopenko <sesopenko@gmail.com>
2016-12-03 17:00:02 -0700

    Install apache2 automatically

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
+
 apt-get update
-apt-get install -y apache2
-if ! [ -L /var/www]; then
-  rm -rf /var/www
-  ln -fs /webroot /var/www
-fi
+apt-get install -y apache2 php7.0 libapache2-mod-php7.0
+ufw allow in "Apache Full"
