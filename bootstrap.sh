b1390052c9ff3a419b59e90a66088a337c202828
Sean Esopenko <sesopenko@gmail.com>
2016-12-20 12:18:19 -0700

    Install & configure xdebug

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,8 @@
 
 
 apt-get update
-apt-get install -y apache2 php7.0 libapache2-mod-php7.0
+apt-get install -y apache2 php7.0 libapache2-mod-php7.0 php-xdebug
 ufw allow in "Apache Full"
+LINE="xdebug.show_error_trace = 1"
+FILE="/etc/php/7.0/mods-available/xdebug.ini"
+grep -q "$LINE" "$FILE" | echo "$LINE" >> "$FILE"
