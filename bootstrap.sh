50624df040b6a2a30e023ae67a2e0342796b02a2
Sean Esopenko <sesopenko@gmail.com>
2016-12-20 14:02:26 -0700

    Add php project dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 
 apt-get update
-apt-get install -y apache2 php7.0 libapache2-mod-php7.0 php-xdebug
+apt-get install -y apache2 php7.0 libapache2-mod-php7.0 php-xdebug php-xml unzip php-zip
 ufw allow in "Apache Full"
 LINE="xdebug.show_error_trace = 1"
 FILE="/etc/php/7.0/mods-available/xdebug.ini"
