e54071b7b102dff34c0b74f51ec972f3a8fc5dda
Sean Esopenko <sesopenko@gmail.com>
2016-12-20 21:37:30 -0700

    Add mysql dependency.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,11 @@
 
 
 apt-get update
-apt-get install -y apache2 php7.0 libapache2-mod-php7.0 php-xdebug php-xml unzip php-zip
+
+echo "mysql-server mysql-server/root_password password password" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password password" | sudo debconf-set-selections
+
+apt-get install -y apache2 php7.0 libapache2-mod-php7.0 php-xdebug php-xml unzip php-zip mysql-server
 ufw allow in "Apache Full"
 LINE="xdebug.show_error_trace = 1"
 FILE="/etc/php/7.0/mods-available/xdebug.ini"
